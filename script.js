4f717f563b1dee5db78b74c103d15b9c2caddce4
Henry Lin <83106787+henrylin03@users.noreply.github.com>
2024-02-20 21:03:18 +1100

    feat: add function to return computer's choice
    use Math.random to randomly select an index of an array of rock, paper, and scissors and return that as the computer's choice

---
+++ script.js
@@ -0,0 +1,8 @@
+function getComputerChoice() {
+    const choicesArray = ["Rock", "Paper", "Scissors"];
+    const randomIndex = Math.floor(Math.random() * choicesArray.length);
+    const choice = choicesArray[randomIndex];
+    return choice;
+};
+
+console.log(getComputerChoice());
