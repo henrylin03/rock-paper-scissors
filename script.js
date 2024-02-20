c6de7d5b79ef4a17aff9cc56bc390ab5ff9e1f26
Henry Lin <83106787+henrylin03@users.noreply.github.com>
2024-02-20 21:19:10 +1100

    feat: add switch/case statements for each playerSelection

--- script.js
+++ script.js
@@ -6,9 +6,53 @@ function getComputerChoice() {
 };
 
 function playRound(playerSelection, computerSelection) {
-    return "hello";
+    let userResult = "";
+
+    //TODO: add function that returns error if user input, even after cleaning, isn't rock, paper or scissors
+
+    if (playerSelection === "rock") {
+        switch(computerSelection) {
+            case "Rock":
+                userResult = "tie";
+                break;
+            case "Scissors":
+                userResult = "win";
+                break;
+            case "Paper":
+                userResult = "lose";
+                break;
+        }
+    } else if (playerSelection === "paper") {
+        switch(computerSelection) {
+            case "Rock":
+                userResult = "win";
+                break;
+            case "Scissors":
+                userResult = "lose";
+                break;
+            case "Paper":
+                userResult = "tie";
+                break;
+        }
+    } else if (playerSelection === "scissors") {
+        switch(computerSelection) {
+            case "Rock":
+                userResult = "lose";
+                break;
+            case "Scissors":
+                userResult = "tie";
+                break;
+            case "Paper":
+                userResult = "win";
+                break
+        }
+    }
+
+    // object that returns string of userResult
+
+    return userResult;
 }
 
-const playerSelection = "rock"; //TODO: convert to user input
+const playerSelection = "rock"; //TODO: convert to user input, trim string, remove spaces, lowercase etc
 const computerSelection = getComputerChoice();
 console.log(playRound(playerSelection, computerSelection));
